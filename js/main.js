56d907f70f785cee29f72b4dcd36ac6ed6573667
AnwaarAhmedshah <fa15be0011@maju.edu.pk>
2020-09-06 23:20:01 +0500

    added array topics with fuctions slice splice shift unshift oush pop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-var x,y,z;
-x=4;
-alert(x);
+/*var age = +prompt("enter value","Value");
+if(age  > 0){
+alert(1);
+}else{
+//alert(2);
+}*/
+
+var arr = ['arr1'];
+
+arr.push('shah','ji');
+arr.pop(); //removes laste index
+arr.unshift('syed anwar shah');//add at beginning
+arr.shift();
+arr.splice(0,0,'syed anwar ahmed shah'); //first arguments is where index to start ending ,2nd argument no of element to remove
+var copy = arr.slice(0,1);
+alert(arr+ ' copy ' + copy);
